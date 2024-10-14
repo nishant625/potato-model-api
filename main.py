8b06bde6ae22e82c08e4f08d19e8349d42c6b474
nishant625 <gangwarnishant625@gmail.com>
2024-10-15 02:02:49 +0530

    last hopefully

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:3000",
-    "https://potato-rose.vercel.app/"
+    "https://potato-rose.vercel.app"
 ]
 
 app.add_middleware(
